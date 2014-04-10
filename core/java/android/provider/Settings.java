89414b974a08b653d746802f13b2c86481d6f017
BigBrother1984 <stevewatersy@gmail.com>
2014-04-10 18:07:26 +0200

    Forward Port: Floating Window with Multitasking.
    Signed-off-by: AudioGod <audiogod@sonic-developers.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4330,6 +4330,12 @@ public final class Settings {
          * @hide
          */
         public static final String RECENTS_CLEAR_ALL_DISMISS_ALL = "recents_clear_all_dismiss_all";
+		
+        /**
+         * Option to launch slim actions in floating mode
+         * @hide
+         */
+        public static final String SLIM_ACTION_FLOATS = "slim_action_floats";
 
 		/**
          * Settings to backup. This is here so that it's in the same place as the settings
