0d20007b3b073aae40e984ee4e771597a0d9e0ec
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-03 02:50:49 +0000

    SystemUI: option to disable lock screen shortcuts (1/2)
    Signed-off-by: AudioGod <adi.nth@outlook.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -32,6 +32,7 @@ import android.os.Bundle;
 import android.os.RemoteException;
 import android.os.UserHandle;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.telecom.TelecomManager;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -221,6 +222,11 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         updateCameraVisibility(); // in case onFinishInflate() was called too early
     }
 
+    private boolean lockScreenShortcutsEnabled() {
+        return Settings.Secure.getInt(mContext.getContentResolver(),
+            Settings.Secure.LOCKSCREEN_BOTTOM_SHORTCUTS, 1) == 1;
+    }
+
     private Intent getCameraIntent() {
         KeyguardUpdateMonitor updateMonitor = KeyguardUpdateMonitor.getInstance(mContext);
         boolean currentUserHasTrust = updateMonitor.getUserHasTrust(
@@ -238,12 +244,13 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
                 PackageManager.MATCH_DEFAULT_ONLY,
                 mLockPatternUtils.getCurrentUser());
         boolean visible = !isCameraDisabledByDpm() && resolved != null
-                && getResources().getBoolean(R.bool.config_keyguardShowCameraAffordance);
+                && getResources().getBoolean(R.bool.config_keyguardShowCameraAffordance)
+                && lockScreenShortcutsEnabled();
         mCameraImageView.setVisibility(visible ? View.VISIBLE : View.GONE);
     }
 
     private void updatePhoneVisibility() {
-        boolean visible = isPhoneVisible();
+        boolean visible = isPhoneVisible() && lockScreenShortcutsEnabled();
         mPhoneImageView.setVisibility(visible ? View.VISIBLE : View.GONE);
     }
 
@@ -370,6 +377,9 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         if (changedView == this && visibility == VISIBLE) {
             updateLockIcon();
             updateCameraVisibility();
+            if (isPhoneVisible()) {
+                updatePhoneVisibility();
+            }
         }
     }
 
