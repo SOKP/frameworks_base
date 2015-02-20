7106a715614f6cd4907633e736c907fff85ba352
Alex Cruz <mazdarider23@gmail.com>
2015-02-20 07:16:49 +0000

    App Circle Bar Tile [1/2]

--- core/java/com/android/internal/util/slim/QSConstants.java
+++ core/java/com/android/internal/util/slim/QSConstants.java
@@ -50,6 +50,7 @@ public class QSConstants {
     public static final String TILE_SCREENSHOT = "screenshot";
     public static final String TILE_LIVE_DISPLAY = "live_display";
     public static final String TILE_SLIMACTION = "slimaction";
+    public static final String TILE_APPCIRCLEBAR = "toggleAppCircleBar";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -90,5 +91,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_SCREENSHOT);
         TILES_AVAILABLE.add(TILE_LIVE_DISPLAY);
         TILES_AVAILABLE.add(TILE_SLIMACTION);
+        TILES_AVAILABLE.add(TILE_APPCIRCLEBAR);
     }
 }
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -64,6 +64,7 @@ import com.android.systemui.qs.tiles.SlimActionTile;
 import com.android.systemui.qs.tiles.VisualizerTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.ScreenTimeoutTile;
+import com.android.systemui.qs.tiles.AppCircleBarTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.settings.CurrentUserTracker;
 import com.android.systemui.statusbar.policy.BluetoothController;
@@ -337,6 +338,8 @@ public class QSTileHost implements QSTile.Host {
                 return new LiveDisplayTile(this);
             case QSConstants.TILE_SLIMACTION:
                 return new SlimActionTile(this);
+            case QSConstants.TILE_APPCIRCLEBAR:
+                return new AppCircleBarTile(this);
             default:
                 throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
         }
