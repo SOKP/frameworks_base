f443d3c014379ae6b5a8006863a9bf1257beffc9
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-03-01 18:54:52 +0000

    SystemUI: long press lock screen lock icon to sleep (1/2)
    Change-Id: Ibb65141ee83fcf7c47e64254c92fd75920c2b185
    Signed-off-by: AudioGod <adi.nth@outlook.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -29,7 +29,9 @@ import android.graphics.drawable.Drawable;
 import android.graphics.drawable.InsetDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.os.RemoteException;
+import android.os.SystemClock;
 import android.os.UserHandle;
 import android.provider.MediaStore;
 import android.provider.Settings;
@@ -99,6 +101,10 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     private final TrustDrawable mTrustDrawable;
     private final Interpolator mLinearOutSlowInInterpolator;
     private int mLastUnlockIconRes = 0;
+
+    private boolean mLongClickToForceLock;
+    private boolean mLongClickToSleep;
+    private PowerManager mPm;
 
     public KeyguardBottomAreaView(Context context) {
         this(context, null);
@@ -302,10 +308,12 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         boolean clickToUnlock = mAccessibilityController.isTouchExplorationEnabled();
         boolean clickToForceLock = mUnlockMethodCache.isTrustManaged()
                 && !mAccessibilityController.isAccessibilityEnabled();
-        boolean longClickToForceLock = mUnlockMethodCache.isTrustManaged()
+        mLongClickToForceLock = mUnlockMethodCache.isTrustManaged()
                 && !clickToForceLock;
+        mLongClickToSleep = Settings.Secure.getIntForUser(getContext().getContentResolver(),
+                Settings.Secure.LONG_PRESS_LOCK_ICON_TO_SLEEP, 0, UserHandle.USER_CURRENT) == 1;
         mLockIcon.setClickable(clickToForceLock || clickToUnlock);
-        mLockIcon.setLongClickable(longClickToForceLock);
+        mLockIcon.setLongClickable(mLongClickToForceLock || mLongClickToSleep);
         mLockIcon.setFocusable(mAccessibilityController.isAccessibilityEnabled());
     }
 
@@ -327,8 +335,14 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
 
     @Override
     public boolean onLongClick(View v) {
-        handleTrustCircleClick();
-        return true;
+        if (mLongClickToSleep && !mLongClickToForceLock) {
+            mPm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+            mPm.goToSleep(SystemClock.uptimeMillis());
+            return true;
+        } else {
+            handleTrustCircleClick();
+            return true;
+        }
     }
 
     private void handleTrustCircleClick() {
