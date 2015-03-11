55d61b6994ef47bb47926ee2cad0aefe74aba3bf
Roman Birg <roman@cyngn.com>
2015-03-11 12:13:05 -0700

    SystemUI: improve DISMISS_KEYGUARD_SECURELY_ACTION behavior
    When the screen is not yet turned on, calls to dismiss() will never
    actually dismiss the keyguard since it goes through the bouncer, which
    checks if the screen is on before executing its logic. If we receive the
    intent to dismiss the keyguard while the screen is off, wait until its
    turned on and then execute the dismiss.
    
    Change-Id: I652458c639230bc7fe32a955e103e40c27289b4d
    Signed-off-by: Roman Birg <roman@cyngn.com>
    Signed-off-by: AudioGod <adi.nth@outlook.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -260,6 +260,8 @@ public class KeyguardViewMediator extends SystemUI {
 
     private boolean mScreenOn;
 
+    private boolean mDismissSecurelyOnScreenOn = false;
+
     // last known state of the cellular connection
     private String mPhoneState = TelephonyManager.EXTRA_STATE_IDLE;
 
@@ -744,6 +746,10 @@ public class KeyguardViewMediator extends SystemUI {
             if (callback != null) {
                 notifyScreenOnLocked(callback);
             }
+            if (mDismissSecurelyOnScreenOn) {
+                mDismissSecurelyOnScreenOn = false;
+                dismiss();
+            }
         }
         KeyguardUpdateMonitor.getInstance(mContext).dispatchScreenTurnedOn();
         maybeSendUserPresentBroadcast();
@@ -1195,7 +1201,11 @@ public class KeyguardViewMediator extends SystemUI {
                         .setPackage(context.getPackageName()));
             } else if (DISMISS_KEYGUARD_SECURELY_ACTION.equals(intent.getAction())) {
                 synchronized (KeyguardViewMediator.this) {
-                    dismiss();
+                    if (mScreenOn) {
+                        dismiss();
+                    } else {
+                        mDismissSecurelyOnScreenOn = true;
+                    }
                 }
             }
         }
