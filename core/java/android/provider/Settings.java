d290bcea79c211ff89ebe6a0828f482d1af2a887
Yashar Ahmedizadeh <yashade2001@gmail.com>
2015-03-23 20:30:56 +0100

    Add an option to disable searchbar in recents [2/2]
    Coded by yashade2001 <yashade2001@gmail.com>
    
    Liquid Edit: Formated Code and squashed into 1 commit
    
    AICP:
    fix logic
    
    Signed-off-by: Yashar Ahmedizadeh <yashade2001@gmail.com>
    Change-Id: I99fe0e2be214fcf55634b36bff2720c0c6265a0f
    Signed-off-by: AudioGod <audiogod@sonic-developers.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3752,6 +3752,12 @@ public final class Settings {
          */
         public static final String PIE_BUTTON_OUTLINE_COLOR = "pie_button_outline_color";
 
+         /**
+         * Recents Show/Hide Search Bar
+         * @hide
+         */
+        public static final String RECENTS_SHOW_HIDE_SEARCH_BAR = "recents_show_hide_search_bar";
+
         /**
          * Pie background color
          * @hide
