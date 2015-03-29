0e3bec1b09f79428dfa6ce5f772e723e1ed06b2b
Steve Kondik <steve@cyngn.com>
2015-03-29 05:29:18 +0200

    livedisplay: Don't depend on automatic brightness

--- services/core/java/com/android/server/display/LiveDisplayController.java
+++ services/core/java/com/android/server/display/LiveDisplayController.java
@@ -256,6 +256,10 @@ public class LiveDisplayController {
         }
     }
 
+    public void updateLiveDisplay() {
+        updateLiveDisplay(mCurrentLux);
+    }
+
     public synchronized void updateLiveDisplay(float lux) {
         mCurrentLux = lux;
         mHandler.removeMessages(MSG_UPDATE_LIVE_DISPLAY);
