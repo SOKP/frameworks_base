03ec2fabeaaad1485779da842b93ee4d085899df
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-04-28 20:42:00 +0100

    Base: Add a controller for in-call proximity sensor (1/3)
    Signed-off-by: AudioGod <AudioGod@Sonic-Developers.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4382,6 +4382,12 @@ public final class Settings {
          * @hide
          */
         public static final String HEADS_UP_FLOATING = "heads_up_floating";
+		
+		/**
+         * Whether to use the proximity sensor to turn the screen on/off during a call
+         * @hide
+         */
+        public static final String IN_CALL_PROXIMITY_SENSOR = "in_call_proximity_sensor";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
