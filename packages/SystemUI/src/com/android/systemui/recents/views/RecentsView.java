b615cc46984e06d7f887d0ebc41294b4f6311d1e
Roman Birg <roman@cyngn.com>
2015-05-13 10:39:07 -0700

    SystemUI: add RecentsView EventLogs
    Change-Id: Iec202e9f764fce85eaa4ddff7c8f311146b00229
    Signed-off-by: Roman Birg <roman@cyngn.com>
    Signed-off-by: AudioGod <audiogod@sonic-developers.com>

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -36,6 +36,7 @@ import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.ViewAnimationUtils;
 import android.view.Gravity;
+import android.util.EventLog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowInsets;
@@ -52,6 +53,7 @@ import com.android.systemui.recents.model.Task;
 import com.android.systemui.recents.model.TaskStack;
 
 import com.android.systemui.R;
+import com.android.systemui.EventLogTags;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -255,6 +257,8 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
             }
         }
         ctx.postAnimationTrigger.decrement();
+
+        EventLog.writeEvent(EventLogTags.SYSUI_RECENTS_EVENT, 1 /* opened */);
     }
 
     /** Requests all task stacks to start their exit-recents animation */
@@ -496,6 +500,8 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
 
                 dismissAllTasksAnimated();
                 updateMemoryStatus();
+
+                EventLog.writeEvent(EventLogTags.SYSUI_RECENTS_EVENT, 4 /* closed all tasks */);
             }
         });
         mMemText = (TextView) ((View)getParent()).findViewById(R.id.recents_memory_text);
@@ -716,6 +722,8 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
                 launchRunnable.run();
             }
         }
+
+        EventLog.writeEvent(EventLogTags.SYSUI_RECENTS_EVENT, 3 /* chose task */);
     }
 
     @Override
@@ -760,6 +768,7 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
                 stackView.onRecentsHidden();
             }
         }
+        EventLog.writeEvent(EventLogTags.SYSUI_RECENTS_EVENT, 2 /* closed */);
     }
 
     @Override
