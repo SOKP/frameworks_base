4572d22bddf5b4b5939c29929eaf2115edb16f3b
Steve Kondik <steve@cyngn.com>
2015-06-19 18:12:19 -0700

    am: Fix delayed GC merge damage, again
     * Unsure how this bug showed up again, as the previous commit shows it
       removed.  Fixing it again to stop the binder stalls.
    
    Change-Id: I8ddde20771695907fdb71482059d46bd69437498
    Signed-off-by: AudioGod <adi.nth@outlook.com>

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -2737,7 +2737,6 @@ public final class ActivityStackSupervisor implements DisplayListener {
                 }
                 final ActivityRecord ar = stack.findTaskLocked(r);
                 if (ar != null) {
-                    BinderInternal.modifyDelayedGcParams();
                     return ar;
                 }
             }
