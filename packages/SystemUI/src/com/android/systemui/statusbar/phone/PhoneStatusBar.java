e09d7166d0549b46ead2c955cdaa1d25cd6c72a3
Shaun Eaton <shaunsibu@gmail.com>
2015-08-23 23:37:37 +0530

    TRDS : Update Colors
    Based on the work of @nicholaschum and was used with his permission.
    
    SOKPify : Remove cm_colors

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3971,7 +3971,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         int uiThemeMode = res.getConfiguration().uiThemeMode;
         if (uiThemeMode != mCurrUiThemeMode) {
             mCurrUiThemeMode = uiThemeMode;
-//            recreateStatusBar(false);
+            recreateStatusBar();
         } else {
             loadDimens();
         }
